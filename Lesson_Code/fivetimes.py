a94f0911491b2ad655422a39379ee2e80c5894e6
Zac Emery <zemery02@gmail.com>
2020-05-07 21:54:51 -0400

    "created section 3, finished lesson 8"

--- Lesson_Code/fivetimes.py
+++ Lesson_Code/fivetimes.py
@@ -1,3 +1,7 @@
 print('My name is')
 for i in range(5):
     print('Jimmy Five Times ' + str(i))
+# You can also use up to 3 arguements for the range function, if you use 2,
+# it will use the first arguement for the starting number, and the second for
+# the point in which the loop will stop. You can use a third arguement to skip
+# numbers and count backwards.
