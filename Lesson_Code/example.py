a16639c9f09a879c485cc385f0ab886484109cef
Zac Emery <zemery02@gmail.com>
2020-05-09 22:44:51 -0400

    "finished lesson nine"

---
+++ Lesson_Code/example.py
@@ -0,0 +1,8 @@
+def hello(name):
+    print('Howdy!')
+    print('Howdy!!!')
+    print('Hello there, ' + name + '. ')
+
+hello("Zac")
+hello("Sam")
+
---
+++ Lesson_Code/plusOneExample.py
@@ -0,0 +1,4 @@
+def plusOne(number):
+    return number + 1
+newNumber = plusOne(68)
+print(newNumber)
